ce7b588f1edbc83a3575f8f48fe0492a44303df4
Marnia Rani <marniarani2289@gmail.com>
2026-07-20 23:13:35 +0700

    Add publications page

---
+++ app/publications/page.tsx
@@ -0,0 +1,9 @@
+import Publications from "../components/Publications";
+
+export default function PublicationsPage() {
+  return (
+    <main className="bg-white">
+      <Publications />
+    </main>
+  );
+}
